49d7e2500b392fbbad6f8fa0f1f1733edbc29e66
Milen Penev <67413841+milen-penev-94@users.noreply.github.com>
2023-06-15 12:07:45 +0300

    Add express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const config = require('./config');
-const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Homepage');
+const config = require('./config');
+const setupViewEngine = require('./config/viewEngine');
+
+const app = express();
+setupViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
