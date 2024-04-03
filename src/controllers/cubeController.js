23429b880d6d949ed13bc86b3485df23bea63867
Milen Penev <67413841+milen-penev-94@users.noreply.github.com>
2024-04-03 16:36:20 +0300

    Add details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const Cube = require('../models/Cube');
+const db = require('../db.json');
 
 exports.getCreateCube = (req,res) => {
     res.render('create');
@@ -13,4 +14,20 @@ exports.postCreateCube = (req, res) => {
 
     // redirect
     res.redirect('/');
+};
+
+exports.getDetails = (req, res) => {
+    let cubeId = Number(req.params.cubeId);
+
+    if (!cubeId) {
+        return res.redirect('404');
+    }
+
+    let cube = db.cubes.find(x => x.id === cubeId);
+
+    if (!cubeId) {
+        return res.redirect('404');
+    }
+
+    res.render('details', { cube });
 };
